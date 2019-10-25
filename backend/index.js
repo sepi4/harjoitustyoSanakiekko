26ec74278f3f22af1104f7d1a4387178e35bbe9d
Sergei <sepolish@student.jyu.fi>
2019-10-25 15:19:44 +0300

    add back folder and start implementin socketio

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 6722
+// const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`)
+})
